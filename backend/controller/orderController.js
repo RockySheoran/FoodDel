fd0a354b45c170f6e5c989017578a93b15def5d5
RockySheoran <rockysheoran72@google.com>
2024-12-27 15:19:11 +0530

    chsfs

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
-  const frontendUrl =  process.env.FRONTEND_URL || "http://localhost:5173" ;
+  const frontendUrl = "http://localhost:5173" ;
 
     // const frontendUrl = "https://fooddel-frontend4.onrender.com";
 
